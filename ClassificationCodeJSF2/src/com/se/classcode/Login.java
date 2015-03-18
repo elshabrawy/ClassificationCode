fc0a7dc03c95aa7a183c6378a2d652cfb525d987
mohamed_elshabrawy <mohamed_elshabrawy@siliconexpert.com>
2015-03-18 16:01:18 +0200

    commit

--- ClassificationCodeJSF2/src/com/se/classcode/Login.java
+++ ClassificationCodeJSF2/src/com/se/classcode/Login.java
@@ -2,12 +2,14 @@ package com.se.classcode;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
 import org.primefaces.context.RequestContext;
 
 @ManagedBean
+@SessionScoped
 public class Login {
 
 	private boolean show = true;
@@ -29,6 +31,29 @@ public class Login {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+    public String onLogin() {
+        RequestContext context = RequestContext.getCurrentInstance();
+//        servle
+        FacesMessage msg = null;
+        boolean loggedIn = false;
+        if (getUsername() != null && getUsername().equals("admin") && getPassword() != null && getPassword().equals("admin")) {
+            loggedIn = true;
+//            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", getUsername());
+//            this.setShow(false);
+            System.out.println("true login");
+            return "SelectedTool?faces-redirect=true";
+        } else {
+            loggedIn = false;
+            this.setShow(false);
+            System.out.println("fals login");
+            msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error", "Invalid credentials");
+            return null;
+        }
+//        System.out.println("" + loggedIn);
+//        FacesContext.getCurrentInstance().addMessage(null, msg);
+//        context.addCallbackParam("loggedIn", loggedIn);
+    }
 
 	public void login(ActionEvent actionEvent) {
 		 RequestContext context = RequestContext.getCurrentInstance();
@@ -37,6 +62,7 @@ public class Login {
        boolean loggedIn = false;
        if (getUsername() != null && getUsername().equals("admin") && getPassword() != null && getPassword().equals("admin")) {
            loggedIn = true;
+           
            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", getUsername());
            this.setShow(false);
            
